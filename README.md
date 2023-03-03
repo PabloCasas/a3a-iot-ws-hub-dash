d2b3da6301860f5f71a66e0df590ccd5f3d9a832
Pablo Jabroni <pablo.alejandro.casas@gmail.com>
2023-03-02 16:46:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# A3a: Websockets-based Hub Dashboard Simulation of local IoT system
+
+**NOTE:** you can view this Markdown (.md) file in the correct formatting using the "preview button" in the top-right corner in Visual Studio Code.  Read the whole document before you continue.
+
+## Overview
+
+Basically, this homework shows you the basics of a local (home) IoT
+system, except everything runs locally on your computer since we have
+not yet quite gotten to making the Bluetooth Low Energy edge node itself.
+This system has three processes, one to simulate each component of
+a local IoT system: an edge node process(es), a home hub process,
+and finally, a user (your phone or computer) client process.  We do
+not have the cloud portion in this simulation yet, but will add it
+in the not-so-distant future.
+
+## System block diagram
+
+![System block diagram](system_diagram.png)
+
+## Packages required
+
+First, make sure to install:
+1. `psutil` via Miniforge Prompt and the ee5450 env: `mamba install psutil` so that we can 
+do the scans for "service discovery".  
+1. `aioconsole` via Miniforge Prompt and the ee5450 env: `mamba install aioconsole` so that we can easily combine asyncio with user input
+1. Insomnia via: https://insomnia.rest/download
+
+Normally you'd use bleak or whatever Bluetooth Low Energy service 
+discovery tool, but since we haven't quite yet gotten to that yet, 
+we'll use `psutil` to discover services on our own computer.  The 
+simulator below will run different services that provide sensor data 
+on different TCP ports above 8080 in order to simulate the discovery 
+process.  The Hub ingester and dashboard work together to get data 
+from the sensor nodes processed and displayed for the homeowner. This 
+setup essentially allows us to simulate a whole local home IoT system 
+on your computer.
+
+Insomnia allows us to test HTTP and WebSockets services without having
+to keep refreshing on the web browser.  You could also just use `aiohttp`
+directly with Python unit tests as well, but it's nice to have some 
+exposure to simpler tools that require less work on your end.  Of course,
+if you were working in industry and trying to make a reliable system
+instead of just trying to finish your homework in a couple of weeks
+
+## Your Tasks
+
+It is up to you to create the command-line interface to the Home Hub services, 
+the Home Hub dashboard client (`app_hub_dash_client.py`), so that it is easy 
+for a user to issue the following commands:
+1. Discover edge nodes
+1. Subscribe to an edge node
+1. Unsubscribe from an edge node
+1. List all currently subscribed edge nodes
+1. Display the values from the last five aggregations from the Hub
+1. Quit the client (disconnect from the hub)
+
+You should be using aiohttp's Client capabilities (both HTTP and WebSockets) so that your client can keep
+receiving data while simultaneously waiting for user commands.
+
+You should also design your program visually first. You may use either sequence diagrams, 
+finite state machine diagrams, flowcharts, and/or base system block diagrams.  
+**NOTE:** You must include your program design diagram to receive credit for this assignment.
+
+**NOTE:** your interface will need to be multitasked using asyncio.  
+The built-in `input()` function is not a coroutine -- it is blocking.
+Thus, you must use the `async_input()` coroutine I've already added to `app_hub_dash_client.py`,
+which shows how it should be used.
+
+## Running the IoT simulation system
+### Executing each program using Visual Studio Code
+
+To run the IoT simulation system, you will need to open three different terminal windows.  
+You can click the + icon in the Terminal window in Visual Studio Code to open additional 
+terminals in your workspace.  You will see three terminals appear in the right side of the
+Terminal pane at the bottom half of your screen.  In each one, make sure the ee5450 environment
+is activated by looking for (ee5450) on the left of the command prompt.
+
+If the `mamba activate ee5450` command fails, you just need to run `conda init` with the
+Miniforge Prompt window, then "trash" all of the terminals.  You will likely need to 
+enable setup scripts by running `Set-ExecutionPolicy -Scope CurrentUser RemoteSigned`.
+
+Then, in each terminal, run the program you want using `python ./{the name of the file here}`.
+
+### Testing the HTTP functions using Insomnia
+
+In Insomnia, you can test the Home Hub by making a new HTTP GET request and using the
+correct URL, along the lines of: `http://localhost:8080/ingester/discover`.  Replace
+the port number with the correct port.
+
+### Testing the WebSockets functions using Insomnia
+
+In Insomnia, you can test the IoT node simulator by making a new WebSocket request 
+and using the URL format: `ws://localhost:8090/data`, where you can replace 8090 with
+8091, 8092, etc. to test a specific node between 8090 and 8099 (the program simulates 
+9 nodes by default).
+
+You can test the Home Hub dashboard by making a new WebSocket request and using the
+URL format: `ws://localhost:8080/hub/dashboard`.  As long as you don't disconnect
+within Insomnia, you will see values from different nodes come up.
---
+++ server_hub_ingester_and_dash.py
@@ -0,0 +1,367 @@
+"""
+IoT Home Hub Server Simulator
+Copyright 2023 by Y. Curtis Wang
+
+This server application is meant to be run on the
+    local home hub.  It serves as an interface
+    between the edge sensor nodes and the homeowner's
+    phones or computers.  Normally this hub would 
+    make yet another connection to some server on the 
+    cloud, but that's just yet another unnecessary 
+    layer once you understand the concepts.
+
+This server application has the following routes:
+
+HTTP GET /ingester/discover : discover and list edge sensor nodes (normally it would be a BLE device scan)
+HTTP GET /ingester/subscribed : list currently subscribed edge sensor nodes
+HTTP GET /ingester/subscribe/{node_id} : subscribe to a node by node_id (which is just going to be a port)
+HTTP GET /ingester/unsubscribe/{node_id} : unsubscribe from a node by node_id
+
+WS GET /hub/dashboard : dashboard the mean values for the characteristics (sensors) of the currently-subscribed nodes
+
+As you can imagine, this means that our server must run a few different tasks simultaneously:
+    1) the Ingester HTTP service (services requests for discovery or subscription), which uses aiohttp
+    2) the Ingester data aggregation task (which actually ingests data and aggregates data), which is just asyncio
+    3) the dashboard WebSockets service (publishes aggregated data to user clients), which uses aiohttp
+
+For more information: https://docs.aiohttp.org/en/stable/web_advanced.html#background-tasks
+"""
+
+
+import asyncio
+import itertools as itt
+import psutil as psu
+import aiohttp.web as aioweb
+import aiohttp as aioh
+from contextlib import suppress
+import time
+from typing import Dict, List
+
+
+class IngesterAggregator(object):
+    """
+    This part of the ingestor subscribes to different WebSockets services
+        that represent edge nodes, to provide a metaphor for what one would
+        do with edge nodes that run on Bluetooth Low Energy.
+    """
+    def __init__(self) -> None:
+        self.period_aggregate_s = 10.0  # aggregate every 10 s
+        self.period_data_timeout_s = 0.1  # run something every 0.1 s
+        self.count_aggregate_time = self.period_aggregate_s / self.period_data_timeout_s
+        self.url_default_path = '/data'  # assume this is the URL for each of our edge nodes
+        self.dict_current_ws_sessions: Dict[int, aioh.ClientSession] = dict()
+        self.dict_current_ws_connections: Dict[int, aioh.ClientWebSocketResponse] = dict()
+        self.dict_current_data_to_aggregate: Dict[int, List[float]] = dict()
+        self.dict_current_aggregates: Dict[int, float] = dict()
+        self.lock_dict_current_aggregates = asyncio.Lock()
+        self.lock_dict_current_ws_connections = asyncio.Lock()
+        self.queue_command = asyncio.Queue(maxsize=1)  # if we keep this 1, adds/removes are synchronous
+
+    async def node_add(self, node_id: int) -> None:
+        """
+        Public async method to subscribe to a new node.
+            Using a command queue prevents potential race conditions.
+            You should attempt to use message passing (command queues)
+            over direct memory modifications as much as possible to avoid 
+            race conditions.
+        
+        :param node_id: the TCP port of the edge node simulator
+        """
+        await self.queue_command.put(("add", node_id))
+
+    async def node_remove(self, node_id: int) -> None:
+        """
+        Public async method to unsubscribe from a node.
+            Using a command queue prevents potential race conditions.
+            You should attempt to use message passing (command queues)
+            over direct memory modifications as much as possible to avoid 
+            race conditions.
+        
+        :param node_id: the TCP port of the edge node simulator
+        """
+        await self.queue_command.put(("remove", node_id))
+
+    async def nodes_subscribed(self) -> list:
+        """
+        Public async method to return the current edge nodes subscribed.
+
+        :returns: list of nodes currently subscribed 
+        """
+        async with self.lock_dict_current_ws_connections:
+            return list(self.dict_current_ws_connections.keys())
+
+    async def get_current_aggregates(self) -> dict:
+        """
+        Public async method to get the current aggregated data.
+
+        :returns: dictionary with nodes as keys and mean (avg) sensor values as values
+        """
+        async with self.lock_dict_current_aggregates:
+            return self.dict_current_aggregates
+
+    async def _node_add(self, node_id: int) -> bool:
+        """
+        The actual method that gets called by the IngesterAggregator's task.
+        NOTE: you should not call this method directly from another task to
+                prevent race conditions.
+        
+        :param node_id: the TCP port of the edge node simulator
+        """
+        async with self.lock_dict_current_ws_connections:
+            self.dict_current_data_to_aggregate[node_id] = list()
+            self.dict_current_ws_sessions[node_id] = aioh.ClientSession()
+            self.dict_current_ws_connections[node_id] = await self.dict_current_ws_sessions[node_id].ws_connect(
+                f'http://localhost:{node_id}{self.url_default_path}'
+            )
+            print(f"IngesterAggregator: Connected to edge node {node_id}!")
+            return node_id in self.dict_current_ws_connections
+
+    async def _node_remove(self, node_id: int) -> bool:
+        """
+        The actual method that gets called to remove a node by this object's task.
+        NOTE: you should not call this method directly.
+
+        :param node_id: the TCP port of the edge node simulator
+        """
+        async with self.lock_dict_current_ws_connections:
+            ws_connection = self.dict_current_ws_connections.pop(node_id, None)
+            if ws_connection is not None:
+                await ws_connection.close()
+            session = self.dict_current_ws_sessions.pop(node_id, None)
+            if session is not None:
+                await session.close()
+            self.dict_current_data_to_aggregate.pop(node_id, None)
+            self.dict_current_aggregates.pop(node_id, None)
+            return node_id not in self.dict_current_ws_connections
+
+    async def _run_process_node_data_dict(self, incoming_data: dict) -> None:
+        """
+        Process incoming WebSocket JSON data from an edge node.
+
+        Assumes data format is:
+        {
+            "node": node_id,
+            "time": UNIX timestamp (seconds since the epoch),
+            "sensor_value": value in float,
+            "sensor_type": str indicating the sensor type
+        }
+
+        :param incoming_data: data published by the websocket
+        """
+        node_id = int(incoming_data["node"])
+        new_val = float(incoming_data["sensor_value"])
+        self.dict_current_data_to_aggregate[node_id].append(new_val)
+
+    async def _run_poll_and_process_new_data(self) -> None:
+        """
+        Polls each currently subscribed edge node for new data.
+        """
+        async with self.lock_dict_current_ws_connections:  # lock this to prevent the for loop from missing something
+            for node, node_ws in self.dict_current_ws_connections.items():
+                try:
+                    async with asyncio.timeout(self.period_data_timeout_s):
+                        the_data = await node_ws.receive_json()
+                        await self._run_process_node_data_dict(the_data)
+                except TimeoutError:
+                    continue
+            
+
+    async def _aggregate_current_data(self) -> None:
+        """
+        Performs the mean on the list of sensor values from each edge node.
+        """
+        async with self.lock_dict_current_aggregates:
+            for node, node_values in self.dict_current_data_to_aggregate.items():
+                self.dict_current_aggregates[node] = sum(node_values) / len(node_values)
+            for node in self.dict_current_data_to_aggregate.keys():
+                self.dict_current_data_to_aggregate[node].clear()
+
+    async def _process_command_queue(self) -> None:
+        """
+        Process the command queue to add or remove subscriptions.        
+        """
+        while not self.queue_command.empty():
+            command = await self.queue_command.get()
+            if command[0] == "add":
+                await self._node_add(command[1])
+            elif command[0] == "remove":
+                await self._node_remove(command[1])
+
+    async def run_aggregator(self) -> None:
+        for the_count in itt.count():  # infinite loop with counter
+            await self._run_poll_and_process_new_data()
+            await self._process_command_queue()
+            if the_count % self.count_aggregate_time == 0:
+                # uncomment the following line if you want to double-check that the task is running
+                await self._aggregate_current_data()
+            else:
+                await asyncio.sleep(self.period_data_timeout_s)
+
+
+class IngesterHTTPService(object):
+    def __init__(self, ref_to_aggregator: IngesterAggregator) -> None:
+        self.ref_to_aggregator = ref_to_aggregator
+        self.periods_to_wait = 5
+        self.timeout_s = self.periods_to_wait * self.ref_to_aggregator.period_data_timeout_s
+
+    async def handle_sub_node(self, request: aioweb.Request) -> aioweb.Response:
+        """
+        HTTP GET handler for subscribing a node.  
+        Returns a JSON dictionary of nodes subscribed.
+        """
+        node_id = int(request.match_info.get('node_id'))
+        await self.ref_to_aggregator.node_add(node_id)
+        await asyncio.sleep(self.timeout_s)
+        nodes_subbed = await self.ref_to_aggregator.nodes_subscribed()
+        return aioweb.json_response(
+            {
+                "success": node_id in nodes_subbed,
+                "nodes_subscribed": nodes_subbed
+            }
+        )
+
+    async def handle_unsub_node(self, request: aioweb.Request) -> aioweb.Response:
+        """
+        HTTP GET handler for unsubscribing a node.
+        Returns a JSON dictionary of nodes subscribed.
+        """
+        node_id = int(request.match_info.get('node_id'))
+        await self.ref_to_aggregator.node_remove(node_id)
+        await asyncio.sleep(self.timeout_s)
+        nodes_subbed = await self.ref_to_aggregator.nodes_subscribed()
+        return aioweb.json_response(
+            {
+                "success": node_id not in nodes_subbed,
+                "nodes_subscribed": nodes_subbed
+            }
+        )
+
+    async def handle_discover_nodes(self, request: aioweb.Request) -> aioweb.Response:
+        """
+        Run the edge node discovery and return dict with discovered nodes as keys  
+            and values of whether the nodes are already subscribed (True) or not.
+
+        NOTE: Assume that local ports 8090-9000 are edge nodes.
+        """
+        response = {
+            "success": False
+        }
+        try:
+            discovered_nodes = [x.laddr.port for x in psu.net_connections(kind='inet') 
+                                if 8090 <= x.laddr.port < 9000 and x.status=='LISTEN']
+            nodes_subbed = await self.ref_to_aggregator.nodes_subscribed()
+            resp_nodes = {node: node in nodes_subbed for node in discovered_nodes}
+            response['success'] = True
+            response["nodes_found"] = resp_nodes
+        except Exception as e:
+            response["error"] = str(e)
+        return aioweb.json_response(response)
+
+
+    async def handle_list_subscribed(self, request: aioweb.Request) -> aioweb.Response:
+        """
+        Return dictionary that includes list of subscribed nodes.
+        """
+        response = {
+            "success": False
+        }
+        try:
+            nodes_subbed = await self.ref_to_aggregator.nodes_subscribed()
+            response['success'] = True
+            response["nodes_subscribed"] = nodes_subbed
+        except Exception as e:
+            response["error"] = str(e)
+        return aioweb.json_response(response)
+
+
+class DashboardWSService(object):
+    """
+    Main class to continuously stream aggregated data to anyone who will listen.
+    """
+    def __init__(self, ref_to_ingester_agg: IngesterAggregator) -> None:
+        self.period_pub_s = 5
+        self.period_refresh_s = 0.5
+        self.count_pub = self.period_pub_s / self.period_refresh_s
+        self.ref_to_ingester_agg = ref_to_ingester_agg
+
+    async def handle_wss_data_stream(self, request: aioweb.Request) -> aioweb.WebSocketResponse:
+        """
+        Handler that infinitely runs until the websocket is closed from the client.
+            Will continue sending data at the rate determined by self.period_pub_s.
+        """
+        ws_resp = aioweb.WebSocketResponse()
+        await ws_resp.prepare(request)
+        for the_count in itt.count():  # infinite loop with counter
+            try:
+                async with asyncio.timeout(self.period_refresh_s):
+                    msg = await ws_resp.receive()
+                    if msg.data == 'close':
+                        await ws_resp.close()
+                    elif msg.type == aioh.WSMsgType.ERROR:
+                        print(f'WebSocket connection closed with exception {ws_resp.exception()}')
+            except RuntimeError:
+                await ws_resp.close()
+                return ws_resp
+            except TimeoutError:
+                pass
+
+            if the_count % self.count_pub == 0:
+                try:
+                    current_data = await self.ref_to_ingester_agg.get_current_aggregates()
+                    await ws_resp.send_json({
+                        "time": round(time.time()),
+                        "data": current_data,
+                        "agg_interval": self.ref_to_ingester_agg.period_aggregate_s
+                    })
+                except ConnectionResetError:
+                    await ws_resp.close()
+                    return ws_resp
+            else:
+                await asyncio.sleep(self.period_refresh_s)
+
+
+class HomeHubApp(object):
+    """
+    The class that ties everything together on the HomeHub. 
+        This is mainly for organization, so we can keep track
+        of all of the objects involved.
+    """
+    def __init__(self) -> None:
+        self.the_ingester_agg = IngesterAggregator()
+        self.the_ingester_http = IngesterHTTPService(self.the_ingester_agg)
+        self.the_dashboard_wss = DashboardWSService(self.the_ingester_agg)
+        self.task_ingester_agg = None
+
+    async def run_non_aiohttp_tasks(self, aiohttp_app: aioweb.Application) -> None:
+        """
+        Runner for the any Task that should run but is not part of the main
+            aiohttp handlers for this server.  In this case, we run the 
+            ingester aggregation task, since it should run at all times.
+        
+        :param aiohttp_app: reference to the main aiohttp_app (unused)
+        """
+        self.task_ingester_agg = asyncio.create_task(self.the_ingester_agg.run_aggregator())
+        yield
+        self.task_ingester_agg.cancel()
+        with suppress(asyncio.CancelledError):
+            await self.task_ingester_agg
+
+
+if __name__ == '__main__':
+    """
+    Using asyncio, our Home Hub Server will be concurrently multitasked, 
+        but still runs on one process.
+    """
+    home_hub = HomeHubApp()
+    app = aioweb.Application()
+    app.cleanup_ctx.append(home_hub.run_non_aiohttp_tasks)
+    app.add_routes(
+        [
+            aioweb.get('/ingester/discover', home_hub.the_ingester_http.handle_discover_nodes),
+            aioweb.get('/ingester/subscribed', home_hub.the_ingester_http.handle_list_subscribed),
+            aioweb.get('/ingester/subscribe/{node_id}', home_hub.the_ingester_http.handle_sub_node),
+            aioweb.get('/ingester/unsubscribe/{node_id}', home_hub.the_ingester_http.handle_unsub_node),
+            aioweb.get('/hub/dashboard', home_hub.the_dashboard_wss.handle_wss_data_stream)
+        ]
+    )
+    aioweb.run_app(app, port=8080)  # NOTE: change the port number if you have LabView or NI MAX installed.
